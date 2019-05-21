4e2700a6929417e0818c6d66fb5c233f44bcecb1
Humberto Ramírez <humberto.rjdh@gmail.com>
2019-05-21 13:35:53 -0500

    add gateWayUrl to dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gateWayUrl: 'http://localhost:8585',
+  dummySession : true
 };
 
 /*
