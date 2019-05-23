77df2f359c56e724ca34d009a8f955cad63170f0
Humberto Ramírez <humberto.rjdh@gmail.com>
2019-05-23 04:36:00 -0500

    setup spanish as locale_id for dev

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Author="Humberto Ramírez Juárez <humberto.rjdh@gmail.com>" 
-ng serve --proxy-config proxy.conf.json --port 4200 -o 
+ng serve --proxy-config proxy.conf.json --port 4200 -o --configuration=es
 
